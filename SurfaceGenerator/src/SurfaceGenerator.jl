4804ebb688628dffa9317829e2ac3c182dad81e8
JingYu Ning <foldfelis@gmail.com>
2021-05-25 18:13:48 +0800

    init project

---
+++ SurfaceGenerator/src/SurfaceGenerator.jl
@@ -0,0 +1,5 @@
+module SurfaceGenerator
+
+# Write your package code here.
+
+end
---
+++ SurfaceGenerator/test/runtests.jl
@@ -0,0 +1,6 @@
+using SurfaceGenerator
+using Test
+
+@testset "SurfaceGenerator.jl" begin
+    # Write your tests here.
+end
